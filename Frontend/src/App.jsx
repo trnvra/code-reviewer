33f6a6048c5041608ca2f20432616e3f2d985855
trnvra <tarunv281@gmail.com>
2026-07-18 00:29:03 +0530

    Update API URL for production

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   async function reviewCode() {
 
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://code-reviewer-s0ya.onrender.com/ai/get-review', { code })
 
     setReview(response.data)
 
