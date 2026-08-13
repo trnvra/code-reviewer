bd3649e00bf9b114ba1dbf0ee5998a8bc60e0648
trnvra <tarunv281@gmail.com>
2026-08-13 23:42:11 +0530

    Fix PrismJS C++ highlighting

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
 import Editor from "react-simple-code-editor";
-
 import Prism from "prismjs";
 
-import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-clike";
+import "prismjs/components/prism-c";
 import "prismjs/components/prism-cpp";
+
+import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-java";
-import "prismjs/components/prism-c";
 import "prismjs/components/prism-csharp";
 import "prismjs/components/prism-markup";
 import "prismjs/components/prism-css";
